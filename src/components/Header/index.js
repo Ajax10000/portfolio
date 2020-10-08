b504c9d5b568c9f8f267130250947853b44e40e7
Ajax10000 <david.de.leon.1000@att.net>
2020-10-08 16:13:16 -0500

    Added Header and Nav components

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../Nav";
+
+function Header() {
+
+    return (
+        <header>
+            <h2 className="float-left">David de Leon</h2>
+            <Nav />
+        </header>
+    )
+}
+
+export default Header;
