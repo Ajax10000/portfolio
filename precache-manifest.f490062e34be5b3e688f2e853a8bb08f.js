a298631c00eff07f5f18635322b07a74d3c4753c
Ajax10000 <david.de.leon.1000@att.net>
2020-10-12 19:18:01 -0500

    Updates

---
+++ precache-manifest.f490062e34be5b3e688f2e853a8bb08f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a57f23c2d2f54877a4431265899a4b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "855623bcb1c4d02027e5",
+    "url": "/portfolio/static/css/main.af39d7b6.chunk.css"
+  },
+  {
+    "revision": "57bb24abc334123644c3",
+    "url": "/portfolio/static/js/2.4088cdf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4088cdf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855623bcb1c4d02027e5",
+    "url": "/portfolio/static/js/main.4ac7a81d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "cbbc8131c296b452e59b4f556b4a9e55",
+    "url": "/portfolio/static/media/elena-mozhvilo-e_lbA4JijqQ-unsplash.cbbc8131.jpg"
+  }
+]);
